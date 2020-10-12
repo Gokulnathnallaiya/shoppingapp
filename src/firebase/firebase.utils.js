d8db33819ceaf8424ddc2c8d069084366d87a455
gokul nath <gokulaps1998@gmail.com>
2020-10-12 18:40:10 +0530

    credentials modified in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBqAqQ8sopUkNJ4l5eHl_ZUiHAR8hGi7N4",
+  authDomain: "ecom-react-c2c57.firebaseapp.com",
+  databaseURL: "https://ecom-react-c2c57.firebaseio.com",
+  projectId: "ecom-react-c2c57",
+  storageBucket: "ecom-react-c2c57.appspot.com",
+  messagingSenderId: "64432453403",
+  appId: "1:64432453403:web:a1701703d30ce12312c5be",
+  measurementId: "G-0TNF7H2DZ8"
 };
 
 firebase.initializeApp(config);
